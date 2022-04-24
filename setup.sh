575cee7103a8c353d3c02da3a7768cf9a1cbbece
Cardboard <31460039+calico-crusade@users.noreply.github.com>
2022-04-23 23:03:18 -0400

    apo? really? It's API!

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker pull docker.pkg.github.com/calico-crusade/osrs-clan-tracker-bot/osrsbot:latest
 docker pull docker.pkg.github.com/calico-crusade/osrs-clan-tracker-bot/osrsui:master
-docker pull docker.pkg.github.com/calico-crusade/osrs-clan-tracker-bot/osrsapo:master
+docker pull docker.pkg.github.com/calico-crusade/osrs-clan-tracker-bot/osrsapi:master
 docker pull kyb3rr/modmail
 
 docker-compose -f docker-compose.yml up -d
